2a1ff7ca8a303247e0bb02003c473c04a4cb5932
kubikos <kubikos63@gmail.com>
2017-01-25 13:08:02 -0800

    projekt3

---
+++ Source.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+typedef struct
+{
+	char *imie;
+	char *nazwisko;
+	int wiek;
+	char *objawa;
+	struct pacjent *next;
+	struct pacjent *prev;
+}pacjent;
+int main()
+{
+
+}
